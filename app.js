5cd69a1266f95aabc82aaed47b4de1e82c8d7ea5
Nathan Bradshaw <nathansbradshaw@gmail.com>
2021-03-16 15:58:26 -0600

    hopefully work with heroku

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,8 +8,16 @@ const errorController = require('./controllers/error');
 
 const app = express();
 
+const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+const corsOptions = {
+   origin: "https://nathan-cse341.herokuapp.com/",
+   optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -23,4 +32,4 @@ app.use(routeRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(PORT);
